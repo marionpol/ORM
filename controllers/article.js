b74123d15e5c6efa4d6e695d4ce91fc69bac1582
Marion Poljakov <marion.poljakov@gmail.com>
2024-09-19 10:27:43 +0300

    Getted article data by slug

--- controllers/article.js
+++ controllers/article.js
@@ -2,9 +2,14 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/orm');
 
 const Article = require('../models/article')(sequelize, Sequelize.DataTypes);
+const Author = require('../models/author')(sequelize, Sequelize.DataTypes);
+
+Article.hasOne(Author);
+
+
 
 const getAllArticles = (req, res) =>{
-    const articles =  Article.findAll()
+    Article.findAll()
     .then(articles => {
         console.log(articles)
         return res.status(200).json({articles});
@@ -15,6 +20,23 @@ const getAllArticles = (req, res) =>{
     
 }
 
+const getArticleBySlug = (req, res) =>{
+    Article.findOne({
+        where: {
+            slug: req.params.slug
+        }
+    })
+    .then(article => {
+        console.log(article)
+        return res.status(200).json({article});
+    })
+    .catch (error => {
+        return res.status(500).send(error.message);
+    })
+    
+}
+
 module.exports = {
-    getAllArticles
+    getAllArticles,
+    getArticleBySlug
 };
