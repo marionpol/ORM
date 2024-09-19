6c2f823364e64a3c730afff42c2f799aa9b3f384
Marion Poljakov <marion.poljakov@gmail.com>
2024-09-19 13:12:27 +0300

    Created new article

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ sequelize
 const articleRouter = require('./routes/article');
 app.use('/', articleRouter);
 app.use('/article', articleRouter);
+app.use('admin/article', articleRouter)
 
 const authorRouter = require('./routes/author');
 app.use('/', authorRouter);
