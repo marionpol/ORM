4a68e496302b3ea1022cce569dec7a98ba022cc2
Marion Poljakov <marion.poljakov@gmail.com>
2024-09-19 10:12:16 +0300

    Getted all articles data

---
+++ controllers/article.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/orm');
+
+const Article = require('../models/article')(sequelize, Sequelize.DataTypes);
+
+const getAllArticles = (req, res) =>{
+    const articles =  Article.findAll()
+    .then(articles => {
+        console.log(articles)
+        return res.status(200).json({articles});
+    })
+    .catch (error => {
+        return res.status(500).send(error.message);
+    })
+    
+}
+
+module.exports = {
+    getAllArticles
+};
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/orm')
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/orm');
 
 sequelize
     .authenticate()
@@ -16,10 +16,8 @@ sequelize
         console.error('Unable to connect to the database', err);
     })
 
-
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to sequelize application."});
-});
+const articleRouter = require('./routes/article');
+app.use('/', articleRouter);
 
 app.listen(3000,() => {
     console.log('Server is running on http://localhost:3000');
